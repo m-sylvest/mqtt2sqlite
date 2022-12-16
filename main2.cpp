dc0ef431f8de12464cffd25167ca0dc17e314e18
Mikael Sylvest <mail@mikael-sylvest.dk>
2022-12-16 18:00:54 +0100

    Now works with sqlite-utils insetion

--- main2.cpp
+++ main2.cpp
@@ -10,44 +10,62 @@ using namespace nlohmann;
 
 typedef struct MQTT2SQLite_stream_t
 {
-    string name;
+    json config;
+    string name, topic;
 
-    typedef function<json(MQTT2SQLite_stream_t &,const json &)> converter_func_t;
     typedef function<bool(MQTT2SQLite_stream_t &,const json &)> filter_func_t;
+    typedef function<json(MQTT2SQLite_stream_t &,const json &)> converter_func_t;
 
-    filter_func_t    filter    = [](auto s,auto j){ return true; };
+    filter_func_t    filter    = [](auto s,auto j){ return false; };
     converter_func_t converter = [](auto s,auto j){ return j; };
 
-    string topic;
-    json config;
-    int valueTypeID = 0;
+    json::json_pointer matchKey, valueKey;
+    json matchValue;
+    int valueTypeID = -1;
+    string deviceName, valueTypeName;
+
+    MQTT2SQLite_stream_t(const json &deviceConfig, const json& valTypeConfig) {
+        config      = valTypeConfig;
+        deviceName  = deviceConfig.value("name","xxx");
+        valueTypeName = valTypeConfig.value("name","yyy");
+        name        = deviceName + "." + valueTypeName;
+        topic       = deviceConfig.value("topic","" );
+        matchKey    = json::json_pointer( valTypeConfig.value("matchKey","" ) );
+        matchValue  = valTypeConfig.value("matchValue", "" );
+        valueKey    = json::json_pointer( valTypeConfig.value("valueKey","" ) );
+        valueTypeID = valTypeConfig.value("ID", -2 );
+
+        clog << "MQTT2SQLite_stream_t(): " << name << endl;
+    };
 
 } MQTT2SQLite_stream_t;
 
 bool filterByJSONP( MQTT2SQLite_stream_t &s,const json &j)
 {
-    json::json_pointer jp( s.config.value("jsonp","") ); 
-    return j.contains(jp);
+//  std::cerr << "filterByJSONP: " << s.name  << ": " << j << endl;
+    bool b1 = j.value("topic","zzz") == s.topic;
+    bool b2 = j.contains(s.valueKey);
+    bool b3 = ( s.matchKey == json::json_pointer("") || j.contains(s.matchKey) && j[s.matchKey] == s.matchValue);
+//  std::cerr << "filterByJSONP: " << b1  << ", " << b2  << ", " << b3  << ", " << endl;
+    return b1 && b2 && b3;
 }
 
 json generateFromJSONP( MQTT2SQLite_stream_t &s,const json &j)
 {
-    json::json_pointer jp( s.config.value("jsonp","") ); 
-    json result{
-        {"value",j[jp] }
+    return json{
+        {"value",    j[s.valueKey]  },
+        {"timeStamp", j.value("tst","2000-01-01T00:00:00.000000Z+0100") },
+        {"valueTypeID", s.valueTypeID }
     };
-    return result;
 }
 
 
 map<string,MQTT2SQLite_stream_t::filter_func_t> filters
 {
-    {"EvasKontor.Temperature", filterByJSONP  }
 };
 
 map<string,MQTT2SQLite_stream_t::converter_func_t> converters
 {
-    {"EvasKontor.Temperature", generateFromJSONP  }
 };
 
 json g_config;
@@ -57,19 +75,21 @@ vector<MQTT2SQLite_stream_t>  makeStreams( json config )
 {
     vector<MQTT2SQLite_stream_t> result;
 
-    for( const auto &dev : config.value("devices",json::array_t{}) )
+    for( auto dev : config.value("devices",json::array_t{}) )
     {
-        for( const auto &valt: dev.value("valueTypes",json::array_t{}) )
+        for( auto valt: dev.value("valueTypes",json::array_t{}) )
         {
-            auto name = dev.value("name","xxx") + "." + valt.value("name","yyy");
-            auto filter_it = filters.find(name);
-            auto filter_func = (filter_it != filters.end()) ? filter_it->second : [](auto s, auto j){ return true; };
+            MQTT2SQLite_stream_t entry(dev,valt);
 
-            auto converter_it = converters.find(name);
-            auto converter_func = (converter_it != converters.end()) ? converter_it->second : [](auto s, auto j){ return true; };
+            auto filter_it = filters.find(entry.name);
+            auto filter_func = (filter_it != filters.end()) ? filter_it->second : filterByJSONP;
+            entry.filter = filter_func;
 
-            MQTT2SQLite_stream_t entry{ name, filter_func, converter_func, "topic", valt, 0 };
-            result.push_back(entry  );
+            auto converter_it = converters.find(entry.name);
+            auto converter_func = (converter_it != converters.end()) ? converter_it->second : generateFromJSONP;
+            entry.converter = converter_func;
+
+            result.push_back(entry);
         } 
     }
     return result;
@@ -88,15 +108,21 @@ int main( int argc, char * argv[] )
         if( line.size() > 0 )
         {
             clog << "Input     :" << line << endl;
-            json parsed = json::parse( line );
-            for( auto &s : g_streams )
-            {
-                if( s.filter(s,parsed) )
+            try {
+                json parsed = json::parse( line );
+                for( auto &s : g_streams )
                 {
-                    clog << "Filtered  :" << parsed << endl;
-                    auto converted = s.converter(s,parsed);
-                    clog << "Converted :" << converted << endl;
+                    if( s.filter(s,parsed) )
+                    {
+                        clog << "Filtered  :" << parsed << endl;
+                        auto converted = s.converter(s,parsed);
+                        clog << "Converted :" << converted << endl;
+                        cout << converted << endl;
+                    }
                 }
+            }
+            catch (std::exception e) {
+                clog << "Exception: " << e.what() << endl;
             }
         }
     }
